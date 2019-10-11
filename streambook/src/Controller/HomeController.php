a7ae0d060dc8cdd749e26f38bbf249efdc782b3a
marylinkana <kanabaudoin7@gmail.com>
2019-10-11 21:36:52 +0200

    get and view book to database

--- streambook/src/Controller/HomeController.php
+++ streambook/src/Controller/HomeController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use http\Client\Curl\User;
+use App\Entity\Books;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class HomeController extends AbstractController
 {
@@ -22,11 +24,56 @@ class HomeController extends AbstractController
             $user = $this->getUser();
             $user_email = $this->getUser()->getEmail();
             $user_id = $this->getUser()->getId();
+            $books = $this->getDoctrine()
+                ->getRepository(Books::class)
+                ->findAll();
             return $this->render('home/index.html.twig', [
                 'controller_name' => 'HomeController',
                 'user' => $user,
                 'user_email' => $user_email,
                 'user_id' => $user_id,
+                'books' => $books,
+            ]);
+        }
+    }
+
+    /**
+     * @Route("/readBook/{id}", name="app_readBook/{id}")
+     */
+    public function readBook($id)
+    {
+        if (is_null($this->getUser())) {
+            return $this->render('home/readBook.html.twig', [
+                'controller_name' => 'HomeController',
+            ]);
+        }
+        else{
+            $user = $this->getUser();
+            $user_email = $this->getUser()->getEmail();
+            $user_id = $this->getUser()->getId();
+            $books = $product = $this->getDoctrine()
+                ->getRepository(Books::class)
+                ->find($id);
+            if (!empty($books)) {
+                $title = $books->getTitle();
+                $description = $books->getDescription();
+                $content = $books->getContent();
+                $url = $books->getUrl();
+            }else{
+                throw $this->createNotFoundException(
+                    'No product found for id '.$id
+                );
+            }
+            return $this->render('home/readBook.html.twig', [
+                'controller_name' => 'HomeController',
+                'user' => $user,
+                'user_email' => $user_email,
+                'user_id' => $user_id,
+                'books' => $books,
+                'bookTitle' => $title,
+                'bookDescription' => $description,
+                'bookContent' => $content,
+                'bookUrl' => $url,
             ]);
         }
     }
