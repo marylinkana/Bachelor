45f7289d4e080cace285208c62d582a11ba1f3aa
marylinkana <kanabaudoin7@gmail.com>
2019-10-11 21:35:42 +0200

    controler of form & submit book

--- streambook/src/Controller/AdminController.php
+++ streambook/src/Controller/AdminController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Books;
+use App\Form\CreateBookFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
 
 class AdminController extends AbstractController
 {
@@ -30,5 +36,33 @@ class AdminController extends AbstractController
                 'admin' => $admin
             ]);
         }
+    }
+
+    /**
+     * @Route("/newbook", name="app_newbook")
+     */
+    public function createBookForm(Request $request): Response
+    {
+        $user = $this->getUser();
+        $user_email = $this->getUser()->getEmail();
+        $user_id = $this->getUser()->getId();
+
+        $book = new Books();
+        $book_form = $this->createForm(CreateBookFormType::class, $book);
+        $book_form->handleRequest($request);
+
+        if ($book_form->isSubmitted()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($book);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('admin/createBook.html.twig', [
+            'bookForm' => $book_form->createView(),
+            'user' => $user,
+            'user_email' => $user_email,
+            'user_id' => $user_id,
+        ]);
     }
 }
